9153a2370ffc80b2abd4b36413369b23a1e8dcf8
surta <surta@DESKTOP-O70IN9U>
2021-02-22 18:46:44 -0800

    Put shit in Socials pl

---
+++ Projects/Socials.pl
@@ -0,0 +1,4 @@
+$instagram = "\@preroutings"; # Set Instagram Variable
+$tiktok = "\@surtains"; # Set TikTok Variable
+
+print "Hi ! Welcome To My First Perl Program.\nMy Name is Tragedy/Surtains. My Socials Are Below !\n\nInstagram - $instagram\nTikTok - $tiktok";
