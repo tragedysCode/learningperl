15c73d6376eae4fc121d2f4fe0cb7bd4a040adb4
surta <surta@DESKTOP-O70IN9U>
2021-02-22 18:58:46 -0800

    Finished Program

--- Projects/Socials.pl
+++ Projects/Socials.pl
@@ -1,4 +1,13 @@
+=
+
+This is a quick program to print my social media's.
+Working on the basics basics (variables, printing, etc.)
+
+=cut
+
 $instagram = "\@preroutings"; # Set Instagram Variable
 $tiktok = "\@surtains"; # Set TikTok Variable
+$snapchat = "daddysurtains"; # Set Snapchat Variable
+$discord = "curtains#3097" # Set Discord Variable
 
-print "Hi ! Welcome To My First Perl Program.\nMy Name is Tragedy/Surtains. My Socials Are Below !\n\nInstagram - $instagram\nTikTok - $tiktok";
+print "Hi ! Welcome To My First Perl Program.\nMy Name is Tragedy/Surtains. My Socials Are Below !\n\nInstagram - $instagram\nTikTok - $tiktok\nSnapchat - $snapchat\Discord - $discord";
